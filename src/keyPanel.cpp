41a65a77ce02a4c57de5a65f1f851b1c8984d8c0
steff393 <stefan.ferstl@gmail.com>
2021-10-05 20:30:15 +0200

    Test of codes (intermediate check-in)

--- src/keyPanel.cpp
+++ src/keyPanel.cpp
@@ -5,33 +5,79 @@
 #include <globalConfig.h>
 #include <Keypad_I2C.h>
 #include <Keypad.h>
+#include <LittleFS.h>
 #include <logger.h>
 #include <Wire.h>
 
-#define CYCLE_TIME                100		// ms
-#define I2C_Addr 0x20                   // I2C Address of PCF8574-board: 0x20 - 0x27
+#define CYCLE_TIME                 100	// ms
+#define I2C_Addr                  0x20  // I2C Address of PCF8574-board: 0x20 - 0x27
+#define KEYP_CODE_MAX               20  // different codes
+#define KEYP_CODE_LEN                7  // 6 digits, e.g. "123456" + string termination
+#define KEYP_NAME_LEN               11  // 10 chars for the person name + string termination
 
-const uint8_t m = 6;
+static const uint8_t m = 6;
 
-const byte ROWS = 4; //four rows
-const byte COLS = 4; //four columns
+static const byte ROWS = 4; //four rows
+static const byte COLS = 4; //four columns
 
 //define the symbols on the buttons of the keypads
-char keyPadLayout[ROWS][COLS] = {
+static char keyPadLayout[ROWS][COLS] = {
 	{'1','2','3','A'},
 	{'4','5','6','B'},
 	{'7','8','9','C'},
 	{'*','0','#','D'}
 };
 
-byte rowPins[ROWS] = {3, 2, 1, 0}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {7, 6, 5, 4}; //connect to the column pinouts of the keypad
+static byte rowPins[ROWS] = {0, 1, 2, 3}; //connect to the row pinouts of the keypad
+static byte colPins[COLS] = {4, 5, 6, 7}; //connect to the column pinouts of the keypad
 
 //initialize an instance of class NewKeypad
-Keypad_I2C i2cKeypad( makeKeymap(keyPadLayout), rowPins, colPins, ROWS, COLS, I2C_Addr); 
+static Keypad_I2C i2cKeypad( makeKeymap(keyPadLayout), rowPins, colPins, ROWS, COLS, I2C_Addr); 
+
+static char code[KEYP_CODE_MAX][KEYP_CODE_LEN];
+static char name[KEYP_CODE_MAX][KEYP_NAME_LEN];
+
+static boolean readCodes() {
+  File file = LittleFS.open(F("/code.txt"), "r");
+  if (!file) {
+    LOG(m, "Disabled (code.txt not found)", "");
+    return(false);
+  }
+
+  uint8_t k = 0;
+  LOGN(m, "Codes: ", "");
+  while (file.available() && k < KEYP_CODE_MAX) {
+    // split the line into tokens limited by ; 
+		char line[KEYP_CODE_LEN + KEYP_CODE_LEN + 5];
+
+
+
+
+		file.readBytesUntil('\n', line, KEYP_CODE_LEN + KEYP_CODE_LEN + 1);
+  	
+		
+		
+		
+		char * pch;
+		pch = strtok(line, ";");
+		//pch = strtok((char*)file.readStringUntil('\n').c_str(), ";");
+    strncpy(code[k], pch, KEYP_CODE_LEN - 1);
+		pch = strtok(NULL, ";");
+		strncpy(name[k], pch, KEYP_NAME_LEN - 1);
+
+    if (k > 0 ) { LOGN(0, ", ", ""); }
+    LOGN(0, "%s-%s", code[k], name[k]);
+    k++;
+  }
+
+  file.close();
+  return(true);
+}
 
 
 void key_setup() {
+	readCodes();
+
 	Wire.begin(PIN_SDA, PIN_SCL);
 	Wire.beginTransmission(I2C_Addr);   // Try to establish connection
 	if (Wire.endTransmission() != 0) {  // No Connection established
@@ -47,6 +93,9 @@ void key_loop() {
 	char key = i2cKeypad.getKey();
 	
 	if (key != NO_KEY) {
-		LOG(m, "%c pressed", key);
+		LOGN(0, "%c,", key);
+	}
+	if (key == 'A') {
+		LOG(0, "-->%c", key);
 	}
 }
